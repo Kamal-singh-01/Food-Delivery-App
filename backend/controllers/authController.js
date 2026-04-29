8a47fe116c80aa328f005f20f528b240cdf9e471
Kamal Singh <kamalsingh902752@gmail.com>
2026-04-29 17:28:34 +0530

    first commit

---
+++ backend/controllers/authController.js
@@ -0,0 +1,67 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/User.js';
+
+// Generate JWT
+const generateToken = (userId) => {
+  return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: '7d' });
+};
+
+//   Register a new user
+//   POST /api/auth/register
+export const registerUser = async (req, res) => {
+  const { name, email, password ,role} = req.body;
+  try {
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
+    const user = await User.create({ name, email, password ,role});
+    if (user) {
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(400).json({ message: 'Invalid user data' });
+    }
+  } catch (error) {
+    console.log("ERROR 👉", error); 
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+// login user
+//POST /api.aut/login
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Find user by email
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+    // Check password
+    const isMatch = await user.matchPassword(password);
+    if (!isMatch) {
+      return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+    res.status(200).json({
+      message: 'Login successful',
+      token: generateToken(user._id),
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error: error.message });
+  }
+};
---
+++ backend/server.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import connectDB from "./config/db.js";
+import authRoutes from './routes/authRoutes.js';
+
+dotenv.config();
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// route
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.use('/api/auth', authRoutes);
+
+// start server AFTER DB connects
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+
+  } catch (error) {
+    console.log("Failed to start server:", error);
+  }
+};
+
+startServer();
